eac4dcc3b51755a5872038238211c76947f0f73b
Raarujeed <mustafaraar@gmail.com>
2018-03-29 10:41:41 +0300

    add more commands to deploy_laravel script

--- deployment/deploy_laravel.sh
+++ deployment/deploy_laravel.sh
@@ -4,18 +4,21 @@
 cd /var/www/html/
 
 # Create cache and chmod folders
-mkdir -p /var/www/html/bootstrap/cache
-mkdir -p /var/www/html/storage/framework/sessions
-mkdir -p /var/www/html/storage/framework/views
-mkdir -p /var/www/html/storage/framework/cache
-mkdir -p /var/www/html/public/files/
+# mkdir -p /var/www/html/bootstrap/cache
+# mkdir -p /var/www/html/storage/framework/sessions
+# mkdir -p /var/www/html/storage/framework/views
+# mkdir -p /var/www/html/storage/framework/cache
+# mkdir -p /var/www/html/public/files/
 
 # Install dependencies
 export COMPOSER_ALLOW_SUPERUSER=1
 composer install -d /var/www/html/
 
 # Copy configuration from /var/www/.env, see README.MD for more information
-cp /var/www/.env /var/www/html/.env
+cp /var/www/html/.env.example /var/www/html/.env
+
+# Generate key envryption
+php /var/www/html/artisan key:generate
 
 # Clear any previous cached views
 php /var/www/html/artisan config:clear
@@ -24,12 +27,13 @@ php /var/www/html/artisan view:clear
 
 # Optimize the application
 php /var/www/html/artisan config:cache
-php /var/www/html/artisan optimize
+# php /var/www/html/artisan optimize
 
 # Change rights
 chmod 777 -R /var/www/html/bootstrap/cache
 chmod 777 -R /var/www/html/storage
-chmod 777 -R /var/www/html/public/files/
+chmod 777 -R /var/www/html/public/
+# chmod 777 -R /var/www/html/public/files/
 
 # Bring up application
 php /var/www/html/artisan up
